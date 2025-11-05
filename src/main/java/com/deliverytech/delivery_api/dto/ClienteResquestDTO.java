88bf1d1fcc5a5f6926573d5c394462b89ab1100e
Diego-Oliveira2005 <deggoliver65@protonmail.com>
2025-11-05 17:40:50 -0300

    entrega 4 100% feita

--- src/main/java/com/deliverytech/delivery_api/dto/ClienteResquestDTO.java
+++ src/main/java/com/deliverytech/delivery_api/dto/ClienteResquestDTO.java
@@ -2,12 +2,14 @@ package com.deliverytech.delivery_api.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size; // 1. Importar
 import lombok.Data;
 
 @Data
-public class ClienteResquetDTO {
+public class ClienteResquestDTO {
 
     @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres") // 2. Adicionar @Size (Bônus)
     private String nome;
 
     @NotBlank(message = "O email é obrigatório")
@@ -15,6 +17,7 @@ public class ClienteResquetDTO {
     private String email;
 
     @NotBlank(message = "O telefone é obrigatório")
+    @Size(min = 10, max = 15, message = "O telefone deve ter entre 10 e 15 caracteres (com DDD)") // 3. Adicionar @Size
     private String telefone;
 
     @NotBlank(message = "O endereço é obrigatório")
